cf53f62be6d30143bcd15fa81890dcabe652d625
high-haseeb <haseebkhalidoriginal@gmail.com>
2024-01-11 18:23:01 +0500

    updated pipes, better dynamics

--- components/Pipes/Experience.jsx
+++ components/Pipes/Experience.jsx
@@ -1,5 +1,9 @@
-"use client"
-import { Environment, OrbitControls, OrthographicCamera } from "@react-three/drei";
+"use client";
+import {
+  Environment,
+  OrbitControls,
+  OrthographicCamera,
+} from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Physics } from "@react-three/rapier";
 import React, { Suspense, useEffect, useState } from "react";
@@ -7,46 +11,64 @@ import Background from "./Background";
 import Walls from "./Walls";
 import Pipes from "./Pipes";
 
+// const getRandomColor = () => {
+//   const colors = ["black", "blue", "darkred"];
+//   return colors[Math.floor(Math.random() * colors.length)];
+// };
 
+let currentIndex = 0;
+const colors = ["black", "blue", "darkred"];
 const getRandomColor = () => {
-  const colors = ["black", "silver", "gold", "darkred"];
-  return colors[Math.floor(Math.random() * colors.length)];
+  const currentColor = colors[currentIndex];
+  currentIndex = (currentIndex + 1) % colors.length; // Cycle to the next color
+  return currentColor;
 };
 
 const useWidth = () => {
-  const [width, setWidth] = useState(0)
-  const [height, setHeight] = useState(0)
-  const handleResize = () => {setWidth(window.innerWidth); setHeight(window.innerHeight)}
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+  const handleResize = () => {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  };
   useEffect(() => {
-      handleResize()
-      window.addEventListener('resize', handleResize)
-      return () => window.removeEventListener('resize', handleResize)
-  }, [])
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return width / height;
-}
+};
 const Experience = () => {
-
   const aspect = useWidth();
   const cameraProps = {
-    left: -10 * aspect,   // Set left boundary of the viewing frustum
-    right: 10 * aspect,   // Set right boundary of the viewing frustum
-    top: 10,     // Set top boundary of the viewing frustum
+    left: -10 * aspect, // Set left boundary of the viewing frustum
+    right: 10 * aspect, // Set right boundary of the viewing frustum
+    top: 10, // Set top boundary of the viewing frustum
     bottom: -10, // Set bottom boundary of the viewing frustum
-    near: 0.01,  // Set near clipping plane
-    far: 10000,  // Set far clipping plane
+    near: 0.01, // Set near clipping plane
+    far: 10000, // Set far clipping plane
   };
   const [pipeColor, setPipeColor] = useState("black");
   return (
     <div className="rounded-3xl w-full my-10 h-full">
       <Suspense fallback={<div>Loading...</div>}>
-        <Canvas className="rounded-3xl" onClick={() => setPipeColor(getRandomColor())}>
-          <OrthographicCamera makeDefault debug position={[0,0,50]} {...cameraProps}/>
+        <Canvas
+          className="rounded-3xl"
+          onClick={() => setPipeColor(getRandomColor())}
+          gl={{ antialias: true, pixelRatio: devicePixelRatio }}
+        >
+          <pointLight color={"red"} intensity={100} postition={[0, 0, 0]} />
+          <OrthographicCamera
+            makeDefault
+            position={[0, 0, 60]}
+            {...cameraProps}
+          />
           <Environment preset="studio" />
-          <Physics gravity={[0,0,0]} colliders={false}>
+          <Physics gravity={[0, 0, 0]} colliders={false}>
             <Walls />
-            <Pipes itemColor={pipeColor} count={15}/>
+            <Pipes itemColor={pipeColor} count={10} />
           </Physics>
-          <Background colorA={pipeColor}/>
+          <Background colorA={pipeColor} />
         </Canvas>
       </Suspense>
     </div>
--- components/Pipes/Pipes.jsx
+++ components/Pipes/Pipes.jsx
@@ -1,77 +1,122 @@
 "use client";
 import { useGLTF } from "@react-three/drei";
-import React, {
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-} from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import {
   BallCollider,
   CuboidCollider,
   InstancedRigidBodies,
 } from "@react-three/rapier";
 import { useFrame } from "@react-three/fiber";
+import * as THREE from "three";
 
-
-
-function Pipes({itemColor, count}) {
-  const [hovered, set] = useState();
-
-  const rigidBodies = useRef(null);
-
-  useEffect(() => {
-    if (!rigidBodies.current) {
-      return;
-    }
-    rigidBodies.current.forEach((api) => {
-      api.applyImpulse({ x: 0, y: -1, z: 0 }, true);
-    });
-  }, []);
+function normalize(value, minValue = -1, maxValue = 1) {
+  return ((value - minValue) / (maxValue - minValue)) * 2 - 1;
+}
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-  useFrame(()=> {
-    if (!rigidBodies.current) {
-      return;
+function Pipes({ itemColor, count }) {
+  const [hovered, set] = useState();
+  const rigidBodies = useRef(null);
+
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const handleMoueMove = (e) => {
+    setMousePosition({
+      x: (e.clientX / window.innerWidth) * 2 - 1,
+      y: (e.clientY / window.innerHeight) * 2 - 1,
+    });
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousemove", (e) => handleMoueMove(e));
+    return document.removeEventListener("mousemove", handleMoueMove);
+  }, []);
+  useEffect(() => {
+    jiggle();
+  }, [itemColor]);
+
+  const centerForceScale = 2;
+  const mouseForceScale = 100;
+  const jiggle = () => {
+    const jiggleScale = 10000;
+    for (let i = 0; i < count; i++) {
+      rigidBodies.current[i].applyImpulse({
+        x: Math.random() * jiggleScale,
+        y: Math.random() * jiggleScale,
+        z: Math.random() * jiggleScale,
+      });
     }
-    for(let i = 0; i < count; i++){
-      if(hovered == i){
-        rigidBodies.current[i].applyImpulse({x:getRandomInt(-100, 100), y:getRandomInt(-100,100),  z:getRandomInt(-100,100)}, true);
+  };
+  useFrame(() => {
+    for (let i = 0; i < count; i++) {
+      let bodyPosition = rigidBodies.current[i].translation();
+      rigidBodies.current[i].applyImpulse({
+        x: -bodyPosition.x * centerForceScale,
+        y: -bodyPosition.y * centerForceScale,
+        // z: -bodyPosition.z * centerForceScale,
+        z:0,
+      });
+      if (hovered == i) {
+        rigidBodies.current[i].applyImpulse(
+          {
+            x: -mousePosition.x * mouseForceScale,
+            y: mousePosition.y * mouseForceScale,
+            z: Math.random() * mouseForceScale,
+          },
+          true,
+        );
       }
     }
-  })
+  });
 
   let SCALE = 1.3;
   const instances = useMemo(() => {
     const instances = [];
 
+    // initialize instances
     for (let i = 0; i < count; i++) {
       instances.push({
         key: "instance_" + Math.random(),
         position: [Math.random() * 10, Math.random() * 10, Math.random() * 10],
-        rotation: [Math.random(), Math.random(), Math.random()],
-        scale:[SCALE, SCALE, SCALE]
+        rotation: [
+          Math.random() * Math.PI,
+          Math.random() * Math.PI,
+          Math.random() * Math.PI,
+        ],
+        scale: [SCALE, SCALE, SCALE],
+        color: 'green'
       });
     }
-
     return instances;
   }, []);
 
-
-  SCALE *= SCALE;
   const { nodes } = useGLTF("/pipe.glb");
-
+  const colliderScale = 2.5;
   return (
-    <InstancedRigidBodies ref={rigidBodies} instances={instances} colliderNodes={[
-      <CuboidCollider args={ [SCALE,1,1] }/>,
-      <CuboidCollider args={ [1,SCALE,1] }/>,
-      <CuboidCollider args={ [1,1,SCALE] }/>
-    ]}  colliders={false} >
-      <instancedMesh args={[nodes.Cross.geometry, undefined, count]}
-      onPointerMove={(e) => (e.stopPropagation(), set(e.instanceId))}
-      onPointerOut={(e) => set(undefined)}>
-        <meshPhysicalMaterial color={itemColor} attach={"material"} roughness={0.3} metalness={0.7}/>
+    <InstancedRigidBodies
+      ref={rigidBodies}
+      instances={instances}
+      colliderNodes={[
+        <CuboidCollider args={[colliderScale, 1, 1]} />,
+        <CuboidCollider args={[1, colliderScale, 1]} />,
+        <CuboidCollider args={[1, 1, colliderScale]} />,
+      ]}
+      linearDamping={2}
+      angularDamping={1}
+      colliders={false}
+    >
+      <instancedMesh
+        args={[nodes.Cross.geometry, undefined, count]}
+        onPointerMove={(e) => (e.stopPropagation(), set(e.instanceId))}
+        onPointerOut={(e) => set(undefined)}
+      >
+
+        <meshPhysicalMaterial
+          attach={"material"}
+          roughness={0.1}
+          metalness={0.9}
+          color={itemColor}
+        />
       </instancedMesh>
     </InstancedRigidBodies>
   );
