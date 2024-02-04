838c0dd42b3976544d32f8719e810bc2d98afc0a
high-haseeb <haseebkhalidoriginal@gmail.com>
2024-02-04 06:35:18 +0500

    bug fix

--- app/page.js
+++ app/page.js
@@ -13,55 +13,50 @@ import FeaturedWork from "@/components/FeaturedWork/FeaturedWork";
 import Connection from "@/components/ConnectingSection/Connection";
 import Experience from "@/components/Experience/Experience";
 import { Planets } from "@/components/Pipes/Planets";
-import { fetchData } from "@/components/Loaders/loader";
 
-// export default function Home() {
-//   const ref = useRef(null);
-//   return (
-//     <Suspense
-//       fallback={
-//         <div className="w-screen bg-black h-screen text-white text-9xl flex items-center justify-center">
-//           Loading...
-//         </div>
-//       }
-//     >
-//       <div className="bg-[#F0F1FA] h-auto w-screen font-[#060607] flex flex-col px-6 lg:px-20 overflow-hidden">
-//         <Navbar />
-//
-//         <div className="h-screen flex flex-col pb-6 ">
-//           <Description />
-//           {/* <div className="h-full bg-brblue rounded-3xl"></div> */}
-//           {/* <Scene className="rounded-xl lg:rounded-3xl" /> */}
-//           <Planets className="h-full rounded-xl lg:rounded-3xl" />
-//           <ScrollText />
-//         </div>
-//       {/* </div> */}
-//         <div className="h-[200vh] relative mt-[20rem]" ref={ref}>
-//           <Skiggle />
-//           <Header />
-//           <SubHeader />
-//           <FeaturedVideo refForward={ref} />
-//         </div>
-//         <FeaturedWork />
-//         <Connection className="" />
-//         <div className="mt-80 w-full h-screen relative">
-//           <Experience className="w-full h-full rounded-3xl" />
-//         </div>
-//       {/* <div className="bg-brblue flex items-center justify-center w-full h-screen font-extrabold text-9xl"> this is the footer  </div> */}
-//         <div className="relative">
-//           <div className="absolute text-9xl font-bold text-center w-full h-full top-80">
-//             HIRE       ME
-//           </div>
-//           <Footer />
-//           <div className="absolute text-3xl font-bold text-center w-full h-full top-[40rem]">
-//             made by haseeb
-//           </div>
-//         </div>
-//       </div>
-//     </Suspense>
-//   );
-// }
 export default function Home() {
-  fetchData("/assets/bunny.buf");
-  return <div>Hello world</div>;
+  const ref = useRef(null);
+  return (
+    <Suspense
+      fallback={
+        <div className="w-screen bg-black h-screen text-white text-9xl flex items-center justify-center">
+          Loading...
+        </div>
+      }
+    >
+      <div className="bg-[#F0F1FA] h-auto w-screen font-[#060607] flex flex-col px-6 lg:px-20 overflow-hidden">
+        <Navbar />
+
+        <div className="h-screen flex flex-col pb-6 ">
+          <Description />
+          {/* <div className="h-full bg-brblue rounded-3xl"></div> */}
+          {/* <Scene className="rounded-xl lg:rounded-3xl" /> */}
+          <Planets className="h-full rounded-xl lg:rounded-3xl" />
+          <ScrollText />
+        </div>
+      {/* </div> */}
+        <div className="h-[200vh] relative mt-[20rem]" ref={ref}>
+          <Skiggle />
+          <Header />
+          <SubHeader />
+          <FeaturedVideo refForward={ref} />
+        </div>
+        <FeaturedWork />
+        <Connection className="" />
+        <div className="mt-80 w-full h-screen relative">
+          <Experience className="w-full h-full rounded-3xl" />
+        </div>
+      {/* <div className="bg-brblue flex items-center justify-center w-full h-screen font-extrabold text-9xl"> this is the footer  </div> */}
+        <div className="relative">
+          <div className="absolute text-9xl font-bold text-center w-full h-full top-80">
+            HIRE       ME
+          </div>
+          <Footer />
+          <div className="absolute text-3xl font-bold text-center w-full h-full top-[40rem]">
+            made by haseeb
+          </div>
+        </div>
+      </div>
+    </Suspense>
+  );
 }
